a031c13ce1e2e7ae8d502906a82e6c4909ffce1e
ye21iin <go_120@naver.com>
2026-03-28 20:58:55 -0700

    design: update metadata for improved title and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,12 @@ import Navbar from "@/components/NavigationBar";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "TimeZone | Plan across time zones",
-  description: "Schedule with friends around the world without worrying about time zones.",
+  title: "OurTimeZone",
+  description: "Manage global times easily",
+  // 이모지를 파비콘으로
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🌍</text></svg>",
+  },
 };
 
 export default function RootLayout({
